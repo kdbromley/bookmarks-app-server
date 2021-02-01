c164cf37f7ffb5dd40081162f7402cc99a6be240
kd Bromley <kd.bromley1@gmail.com>
2021-02-01 13:04:31 -0800

    post /bookmarks req.body data validation

--- src/app.js
+++ src/app.js
@@ -68,6 +68,23 @@ app.get('/bookmarks/:id', (req, res) => {
     res.json(bookmark)
 })
 
+app.post('/bookmarks', (req, res) => {
+    const { title, url, rating=0, desc='' } = req.body;
+    if(!title) {
+        logger.error('Title is required');
+        return res.status(400).send('Invalid data');
+    }
+    if(!url) {
+        logger.error('URL is required');
+        return res.status(400).send('Invalid data');
+    };
+    if (!url.match(/^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/)) {
+        logger.error('Invalid url');
+        return res.status(400).send('Invalid data')
+    }
+    res.status(200).send('ok')
+})
+
 app.use(function errorHandler(error, req, res, next) {
     let response;
     if (NODE_ENV === 'production') {
