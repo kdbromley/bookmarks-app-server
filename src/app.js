7161157fb312a52dd38dbcd993a5ea99bd7d4d05
kd Bromley <kd.bromley1@gmail.com>
2021-02-01 12:24:30 -0800

    add winston logger

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const winston = require('winston');
 
 
 const app = express();
@@ -17,15 +18,30 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'info.log' })
+    ]
+});
+
+if (NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }));
+};
+
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN;
     const authToken = req.get('Authorization');
 
     if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path ${req.path}`)
         return res.status(401).json({ error: 'Unauthorized request' });
     };
     next()
-})
+});
 
 const bookmarks = [{
     id: '875d3650-2225-4144-b9ab-652df8772421',
